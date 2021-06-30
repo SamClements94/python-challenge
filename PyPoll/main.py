4d0c505b4c670a180926ba6a02c8490e574cbd45
Sam Clements <82127462+SamClements94@users.noreply.github.com>
2021-06-29 21:33:22 -0700

    Final

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -46,30 +46,14 @@ print("_______________________")
 file_name = "election.txt"
 with open(os.path.join("Analysis",file_name), "w") as txt_file:
 
+    
     txt_file.write("Election Results\n")
     txt_file.write("_______________________\n")
     txt_file.write(f"Total Votes: {total_votes}\n")
     txt_file.write("_______________________\n")
-    txt_file.write(f" {key} : ({round(votes_percent, 2)}%) {str(value)}\n")
+    for key, value in election.items():
+        txt_file.write(f" {key} : ({round(votes_percent, 2)}%) {str(value)}\n")
     txt_file.write("_______________________\n")
     txt_file.write(f"Winner: {winner_name}\n")
     txt_file.write ("_______________________\n") 
     
-
-        
-        
-        
-# The total number of votes cast
-
-
-
-# A complete list of candidates who received votes
-
-
-# The percentage of votes each candidate won
-
-
-# The total number of votes each candidate won
-
-
-# The winner of the election based on popular vote.
